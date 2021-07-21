7c022444e5b7857aed774a12061dce4d0d5d8d9b
Shark7834 <Shark43255@gmail.com>
2021-07-21 22:23:27 +0300

    dz2Asp

--- dz1Plakas/Controllers/PagesController.cs
+++ dz1Plakas/Controllers/PagesController.cs
@@ -43,6 +43,111 @@ namespace dz1Plakas.Controllers
             return View(page);
         }
 
-       
+        // GET: Pages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,title,body,seokey,seods,ing")] Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(page);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(page);
+        }
+
+        // GET: Pages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.pages.FindAsync(id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+            return View(page);
+        }
+
+        // POST: Pages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,title,body,seokey,seods,ing")] Page page)
+        {
+            if (id != page.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(page);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PageExists(page.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(page);
+        }
+
+        // GET: Pages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.pages
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // POST: Pages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var page = await _context.pages.FindAsync(id);
+            _context.pages.Remove(page);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PageExists(int id)
+        {
+            return _context.pages.Any(e => e.id == id);
+        }
     }
 }
